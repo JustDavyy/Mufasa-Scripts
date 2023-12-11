8cf3af715f37ef40e58b56eda5a860e307a5726b
Davy <davykanningstudent@gmail.com>
2023-12-11 19:45:26 +0100

    Fletcher script?

--- AIO Bow Fletcher/src/main/java/AIOBowFletcher.java
+++ AIO Bow Fletcher/src/main/java/AIOBowFletcher.java
@@ -3,12 +3,15 @@ import helpers.utils.OptionType;
 
 import java.awt.*;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Random;
 
 @ScriptManifest(
         name = "AIO Bow Fletcher",
-        description = "AIO Bow Fletcher, supports both cutting and strining bows.",
+        description = "AIO Bow Fletcher, supports both cutting and stringing bows.",
         version = "1.0",
-        category = ScriptCategory.Combat
+        category = ScriptCategory.Fletching
 )
 @ScriptConfiguration.List(
         {
@@ -17,8 +20,8 @@ import java.util.Map;
                         description = "Which operation would you like to perform?",
                         defaultValue = "Cut",
                         allowedValues = {
-                                @AllowedValue(optionName = "Cut"), // itemID not specified
-                                @AllowedValue(optionName = "String"), // itemID not specified
+                                @AllowedValue(optionName = "Cut"),
+                                @AllowedValue(optionName = "String")
                         },
                         optionType = OptionType.STRING
                 ),
@@ -35,35 +38,258 @@ import java.util.Map;
                                 @AllowedValue(optionIcon = "1513", optionName = "Magic logs")
                         },
                         optionType = OptionType.STRING
+                ),
+                @ScriptConfiguration(
+                        name =  "Product",
+                        description = "Would you like to make short or longbows?",
+                        defaultValue = "Longbow",
+                        allowedValues = {
+                                @AllowedValue(optionName = "Shortbow"),
+                                @AllowedValue(optionName = "Longbow")
+                        },
+                        optionType = OptionType.STRING
                 )
         }
 )
 
 public class AIOBowFletcher extends AbstractScript {
-    String chosenTest; //Lets save the 1st config value
-    String anotherConfig; //Lets save the 2nd config value
+    // Creating the strings for later use
+    String method;
+    String tier;
+    String product;
     String bankloc;
+    String initialsetup;
+    String logs;
+    String shortbowU;
+    String longbowU;
+    String shortbow;
+    String longbow;
+    String bowstring = "1777";
+    String knife = "946";
+    private Map<String, String[]> itemIDs;
 
     @Override
     public void onStart(){
         Map<String, String> configs = getConfigurations(); //Get the script configuration
-        chosenTest = configs.get("Test configuration"); //Set this value to the 'chosenTest' string
-        anotherConfig = configs.get("Another configuration"); //Get the value from the 2nd configuration option
+        method = configs.get("Method");
+        tier = configs.get("Tier");
+        product = configs.get("Product");
+
+        initializeItemIDs();
 
         //Logs for debugging purposes
-        logger.log("We are starting the sample script and running onStart()");
-        logger.log("Test configuration set to: " + chosenTest);
-        logger.log("2nd config value set to: " + anotherConfig);
-        System.out.println("Starting the SampleScript!");
+        logger.log("Thank you for using the AIO Bow Fletcher script!");
+        System.out.println("Starting the AIO Bow Fletcher script!");
+    }
+
+    private void initializeItemIDs() {
+        itemIDs = new HashMap<>();
+
+        // Map of itemIDs for LogID (1), UnstrungShortbowID (2), UnstrungLongbowID (3), StrungShortbowID (4) and StrungLongbowID (5)
+        itemIDs.put("Logs", new String[] {"1511", "50", "48", "841", "839"});
+        itemIDs.put("Oak logs", new String[] {"1521", "54", "56", "843", "845"});
+        itemIDs.put("Willow logs", new String[] {"1519", "60", "58", "849", "847"});
+        itemIDs.put("Maple logs", new String[] {"1517", "64", "62", "853", "851"});
+        itemIDs.put("Yew logs", new String[] {"1515", "68", "66", "857", "855"});
+        itemIDs.put("Magic logs", new String[] {"1513", "72", "70", "861", "859"});
     }
 
     @Override
     public void poll() {
-        logger.log("Starting the poll method for AIO Bow Fletcher.");
+        // Setting the correct IDs to run the script
+        if (longbow == null) {
+            String[] itemIds = itemIDs.get(tier);
+            logs = itemIds[1];
+            shortbowU = itemIds[2];
+            longbowU = itemIds[3];
+            shortbow = itemIds[4];
+            longbow = itemIds[5];
 
-        // Main logic
-        logger.log("Executing the main logic for AIO Bow Fletcher.");
-        condition.sleep(2000);
-        xpBar.getXP();
+            System.out.println("Stored IDs for " + tier + ":\nLogs: " + logs + "\nUnstrung Shortbow: " + shortbowU + "\nUnstrung Longbow: " + longbowU + "\nShortbow: " + shortbow + "\nLongbow: " + longbow);
+        }
+
+        // Dynamic banking (and initially open the bank)
+        if (bankloc == null) {
+
+            // Opening the inventory if not yet opened.
+            logger.debugLog("Opening up the inventory.");
+            if (!gameTabs.isInventoryTabOpen()) {
+                gameTabs.openInventoryTab();
+            }
+
+            logger.debugLog("Starting setup for Dynamic Banking.");
+            bankloc = bank.setupDynamicBank();
+            logger.log("We're located at: " + bankloc + ".");
+            condition.sleep(5000);
+            logger.debugLog("Attempting to open the Bank of Gielinor.");
+            bank.open(bankloc);
+            condition.wait(() -> bank.isOpen(), 250, 12);
+            logger.debugLog("Bank interface detected!");
+            if (bank.isBankPinNeeded()) {
+                logger.log("Bank pin is needed!");
+                bank.enterBankPin();
+                condition.sleep(2000);
+                logger.log("Bank pin entered.");
+            } else {
+                logger.log("Bank pin is not needed, bank is open!");
+            }
+        }
+
+        // Check if knife is present in Inventory, otherwise withdraw it from the bank.
+        if (!inventory.contains(knife, 0.90)) {
+            logger.debugLog("Inventory doesn't contain a knife, withdrawing it from the bank.");
+            bank.tapQuantity1Button();
+            condition.wait(() -> bank.isSelectedQuantity1Button(), 500, 10);
+            bank.tapSearchButton();
+            condition.sleep(1000);
+
+            // Send keystroke for each character
+            String textToSend = "knife";
+            for (char c : textToSend.toCharArray()) {
+                String keycode = "KEYCODE_" + Character.toUpperCase(c);
+                client.sendKeystroke(keycode);
+            }
+
+            condition.sleep(3000);
+            bank.withdrawItem(knife, 0.90);
+            condition.sleep(500);
+
+            // Send keystroke to close the search interface
+            client.sendKeystroke("KEYCODE_ENTER");
+            condition.sleep(1000);
+        }
+
+        // Specific setup for "cut" method
+        if (Objects.equals(method, "Cut")) {
+            logger.log("Cut method was selected.");
+            if (!bank.isSelectedQuantityAllButton()) {
+                bank.tapQuantityAllButton();
+                condition.wait(() -> bank.isSelectedQuantityAllButton(), 1000, 5);
+
+                // Withdraw first set of items
+                bank.withdrawItem(logs, 0.90);
+            }
+        }
+
+        // Specific setup for "string" method
+        if (Objects.equals(method, "String")){
+            logger.log("String method was selected.");
+            if (!bank.isSelectedQuantityCustomButton()) {
+                Rectangle customQty = bank.findQuantityCustomButton();
+                client.longPressWithinRectangle(customQty);
+                condition.sleep(500);
+                client.tap(393, 499);
+                condition.sleep(1000);
+                client.sendKeystroke("KEYCODE_1");
+                client.sendKeystroke("KEYCODE_4");
+                client.sendKeystroke("KEYCODE_ENTER");
+
+                condition.wait(() -> bank.isSelectedQuantityCustomButton(), 500, 10);
+
+                // Withdraw first set of items
+                if (Objects.equals(product, "Shortbow")) {
+                    bank.withdrawItem(shortbowU, 0.90);
+                } else {
+                    bank.withdrawItem(longbowU, 0.90);
+                }
+                bank.withdrawItem(bowstring, 0.90);
+            }
+        }
+
+        // End the initial setup
+        if (!Objects.equals(initialsetup, "done")) {
+            logger.debugLog("Closing bank interface.");
+            bank.close();
+            condition.sleep(1500);
+            initialsetup = "done";
+        }
+
+        // ---------------------------------------------------------------- //
+
+        // Main script logic for cut method
+        if (Objects.equals(method, "Cut")) {
+            inventory.tapItem(knife, 0.90);
+            int randomDelay = new Random().nextInt(251) + 50;
+            int randomBiggerDelay = new Random().nextInt(200) + 400;
+            condition.sleep(randomDelay);
+            inventory.tapItem(logs, 0.90);
+            condition.wait(() -> chatbox.isMakeMenuVisible(), 250, 12);
+
+            // tap option needed based on choice in config
+            if (Objects.equals(product, "Shortbow")) {
+                chatbox.makeOption(2);
+            } else {
+                chatbox.makeOption(3);
+            }
+
+            // Wait for the inventory to finish
+            while (inventory.contains(logs, 0.90)) {
+                xpBar.getXP();
+                int randomDelay2 = new Random().nextInt(2000) + 1000;
+                condition.sleep(randomDelay2);
+            }
+            xpBar.getXP();
+
+            bank.open(bankloc);
+            condition.wait(() -> bank.isOpen(), 250, 12);
+            // bank item needed based on choice in config
+            if (Objects.equals(product, "Shortbow")) {
+                inventory.tapItem(shortbowU, 0.90);
+            } else {
+                inventory.tapItem(longbowU, 0.90);
+            }
+            condition.sleep(randomBiggerDelay);
+
+            bank.withdrawItem(logs, 0.90);
+            condition.sleep(randomDelay);
+            bank.close();
+            condition.sleep(randomBiggerDelay);
+        }
+
+        // Main script logic for string method
+        if (Objects.equals(method, "String")){
+            if (Objects.equals(product, "Shortbow")) {
+                inventory.tapItem(shortbowU, 0.90);
+            } else {
+                inventory.tapItem(longbowU, 0.90);
+            }
+            int randomDelay = new Random().nextInt(251) + 50;
+            int randomBiggerDelay = new Random().nextInt(200) + 400;
+            condition.sleep(randomDelay);
+            inventory.tapItem(bowstring, 0.90);
+            condition.wait(() -> chatbox.isMakeMenuVisible(), 250, 12);
+            chatbox.makeOption(1);
+
+            // Wait for the inventory to finish
+            while (inventory.contains(bowstring, 0.90)) {
+                xpBar.getXP();
+                int randomDelay2 = new Random().nextInt(500) + 500;
+                condition.sleep(randomDelay2);
+            }
+            xpBar.getXP();
+
+            bank.open(bankloc);
+            condition.wait(() -> bank.isOpen(), 250, 12);
+            // bank item needed based on choice in config
+            if (Objects.equals(product, "Shortbow")) {
+                inventory.tapItem(shortbow, 0.90);
+            } else {
+                inventory.tapItem(longbow, 0.90);
+            }
+            condition.sleep(randomBiggerDelay);
+
+            if (Objects.equals(product, "Shortbow")) {
+                bank.withdrawItem(shortbowU, 0.90);
+            } else {
+                bank.withdrawItem(longbowU, 0.90);
+            }
+            condition.sleep(randomDelay);
+            bank.withdrawItem(bowstring, 0.90);
+            condition.sleep(randomDelay);
+            bank.close();
+            condition.sleep(randomBiggerDelay);
+        }
+
+        // ---------------------------------------------------------------- //
     }
 }
