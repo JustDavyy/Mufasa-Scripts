bbcec0292714ff72b9bcdc279d7bf076da76f374
Davy <davykanningstudent@gmail.com>
2024-02-11 12:44:38 +0100

    Gem cutter v1.00

---
+++ dGemCutter/src/main/java/dGemCutter.java
@@ -0,0 +1,401 @@
+import helpers.*;
+import helpers.utils.OptionType;
+
+import java.util.Map;
+import java.util.HashMap;
+import static helpers.Interfaces.*;
+import static helpers.Interfaces.Logout;
+
+import java.util.Random;
+
+@ScriptManifest(
+        name = "dGem Cutter",
+        description = "Cuts any uncut into their cut variants, supports dynamic banking and world hops.",
+        version = "1.00",
+        guideLink = "https://wiki.mufasaclient.com/docs/dgem-cutter/",
+        categories = {ScriptCategory.Crafting}
+)
+@ScriptConfiguration.List(
+        {
+                @ScriptConfiguration(
+                        name =  "Product",
+                        description = "What uncut type would you like to process?",
+                        defaultValue = "Uncut ruby",
+                        allowedValues = {
+                                @AllowedValue(optionIcon = "1625", optionName = "Uncut opal"),
+                                @AllowedValue(optionIcon = "1627", optionName = "Uncut jade"),
+                                @AllowedValue(optionIcon = "1629", optionName = "Uncut red topaz"),
+                                @AllowedValue(optionIcon = "1623", optionName = "Uncut sapphire"),
+                                @AllowedValue(optionIcon = "1621", optionName = "Uncut emerald"),
+                                @AllowedValue(optionIcon = "1619", optionName = "Uncut ruby"),
+                                @AllowedValue(optionIcon = "1617", optionName = "Uncut diamond"),
+                                @AllowedValue(optionIcon = "1631", optionName = "Uncut dragonstone"),
+                                @AllowedValue(optionIcon = "6571", optionName = "Uncut onyx"),
+                                @AllowedValue(optionIcon = "19496", optionName = "Uncut zenyte")
+                        },
+                        optionType = OptionType.STRING
+                ),
+                @ScriptConfiguration(
+                        name =  "Bank Tab",
+                        description = "What bank tab are your resources located in?",
+                        defaultValue = "0",
+                        optionType = OptionType.BANKTABS
+                ),
+                @ScriptConfiguration(
+                        name =  "Use world hopper?",
+                        description = "Would you like to hop worlds based on your hop profile settings?",
+                        defaultValue = "0",
+                        optionType = OptionType.WORLDHOPPER
+                )
+        }
+)
+
+public class dGemCutter extends AbstractScript {
+    String hopProfile;
+    Boolean hopEnabled;
+    Boolean useWDH;
+    String product;
+    String bankloc;
+    int banktab;
+    String chisel = "1755";
+    String crushedgem = "1633";
+    String unprocessedItemID;
+    String processedItemID;
+    Map<String, String[]> ItemIDs;
+
+    // This is the onStart, and only gets ran once.
+    @Override
+    public void onStart(){
+        Map<String, String> configs = getConfigurations();
+        product = configs.get("Product");
+        banktab = Integer.parseInt(configs.get("Bank Tab"));
+        hopProfile = (configs.get("Use world hopper?"));
+        hopEnabled = Boolean.valueOf((configs.get("Use world hopper?.enabled")));
+        useWDH = Boolean.valueOf((configs.get("Use world hopper?.useWDH")));
+
+        initializeItemIDs();
+
+        // Initialize hop timer for this run if hopping is enabled
+        hopActions();
+
+        // One-time setup
+        setupItemIDs();
+        setupBanking();
+        initialSetup();
+
+        //Logs for debugging purposes
+        Logger.log("Thank you for using the dGem Cutter script!");
+        Logger.log("Setting up everything for your gains now...");
+    }
+
+    // This is the main part of the script, poll gets looped constantly
+    @Override
+    public void poll() {
+
+        checkInventOpen();
+        checkInventUncuts();
+        executeCutting();
+        bank();
+        hopActions();
+
+    }
+
+    private void initializeItemIDs() {
+        Logger.debugLog("Running the initializeItemIDs() method.");
+
+        ItemIDs = new HashMap<>();
+
+        // Map of item names to their IDs, where the first ID is unprocessed, and the second ID is the processed ID
+        ItemIDs.put("Uncut opal", new String[]{"1625", "1609"});
+        ItemIDs.put("Uncut jade", new String[]{"1627", "1611"});
+        ItemIDs.put("Uncut red topaz", new String[]{"1629", "1613"});
+        ItemIDs.put("Uncut sapphire", new String[]{"1623", "1607"});
+        ItemIDs.put("Uncut emerald", new String[]{"1621", "1605"});
+        ItemIDs.put("Uncut ruby", new String[]{"1619", "1603"});
+        ItemIDs.put("Uncut diamond", new String[]{"1617", "1601"});
+        ItemIDs.put("Uncut dragonstone", new String[]{"1631", "1615"});
+        ItemIDs.put("Uncut onyx", new String[]{"6571", "6573"});
+        ItemIDs.put("Uncut zenyte", new String[]{"19496", "19493"});
+
+        Logger.debugLog("Ending the initializeItemIDs() method.");
+    }
+
+    private void setupItemIDs() {
+        Logger.debugLog("Running the setupItemIDs() method.");
+
+        if (ItemIDs.containsKey(product)) {
+            String[] ids = ItemIDs.get(product);
+            if (ids != null && ids.length == 2) {
+                unprocessedItemID = ids[0];
+                processedItemID = ids[1];
+
+                // Use these IDs as needed
+                Logger.debugLog("\nUnprocessed item ID: " + unprocessedItemID + "\nProcessed item ID: " + processedItemID);
+            } else {
+                Logger.debugLog("IDs not found for product: " + product);
+            }
+        } else {
+            Logger.debugLog("Product not found: " + product);
+        }
+
+        Logger.debugLog("Ending the setupItemIDs() method.");
+    }
+
+    private void setupBanking() {
+        Logger.debugLog("Starting setupBanking() method.");
+        if (bankloc == null) {
+            Logger.debugLog("Starting dynamic banking setup...");
+
+            // Opening the inventory if not yet opened.
+            Logger.debugLog("Opening up the inventory.");
+            if (!GameTabs.isInventoryTabOpen()) {
+                GameTabs.openInventoryTab();
+            }
+
+            Logger.debugLog("Starting setup for Dynamic Banking.");
+            bankloc = Bank.setupDynamicBank();
+            Logger.log("We're located at: " + bankloc + ".");
+            Logger.debugLog("We're located at: " + bankloc + ".");
+            if (bankloc == null) {
+                Logger.debugLog("Could not find a dynamic bank location we are in, logging out and aborting script.");
+                Logout.logout();
+                Script.forceStop();
+            }
+            Condition.sleep(5000);
+            Logger.debugLog("Attempting to open the Bank of Gielinor.");
+            Bank.open(bankloc);
+            Logger.debugLog("Bank interface detected!");
+            if (Bank.isBankPinNeeded()) {
+                Logger.debugLog("Bank pin is needed!");
+                Bank.enterBankPin();
+                Condition.sleep(500);
+                Condition.wait(() -> Bank.isOpen(), 200, 12);
+                Logger.debugLog("Bank pin entered.");
+                Logger.debugLog("Depositing inventory.");
+                Bank.tapDepositInventoryButton();
+                Condition.sleep(706);
+            } else {
+                Logger.debugLog("Bank pin is not needed, bank is open!");
+                Logger.debugLog("Depositing inventory.");
+                Bank.tapDepositInventoryButton();
+                Condition.sleep(699);
+            }
+        }
+        Logger.debugLog("Ending the setupBanking() method.");
+    }
+
+    private void initialSetup() {
+        Logger.debugLog("Starting initialSetup() method.");
+
+        int randomDelay = new Random().nextInt(600) + 600;
+        int randomBiggerDelay = new Random().nextInt(1500) + 1500;
+
+        // Withdrawing a chisel from the bank
+        Logger.debugLog("Withdrawing a chisel from the bank.");
+        if (!Bank.isSelectedQuantity1Button()) {
+            Bank.tapQuantity1Button();
+            Condition.wait(() -> Bank.isSelectedQuantity1Button(), 200, 12);
+        }
+        Bank.tapSearchButton();
+        Condition.sleep(randomDelay);
+
+        String textToSend = "chisel";
+        for (char c : textToSend.toCharArray()) {
+            String keycode = "KEYCODE_" + Character.toUpperCase(c);
+            Client.sendKeystroke(keycode);
+            Logger.debugLog("Sent keystroke: " + keycode);
+        }
+
+        Condition.sleep(randomBiggerDelay);
+        Bank.withdrawItem(chisel, 0.75);
+        Condition.sleep(randomDelay);
+        Logger.debugLog("Withdrew chisel from the bank.");
+
+        Client.sendKeystroke("KEYCODE_ENTER");
+        Condition.sleep(randomDelay);
+        Logger.debugLog("Closed search interface.");
+
+        // Check if we have the chisel in the inventory, otherwise stop script.
+        Condition.wait(() -> Inventory.contains(chisel, 0.75), 250,10);
+        if (!Inventory.contains(chisel, 0.75)) {
+            Logger.log("No chisel found in inventory, assuming we're out of items to process.");
+            Bank.close();
+            if (Bank.isOpen()) {
+                Bank.close();
+            }
+            Logout.logout();
+            Script.forceStop();
+        }
+
+        // Grabbing the first uncuts to process
+        if (!Bank.isSelectedQuantityAllButton()) {
+            Bank.tapQuantityAllButton();
+            Condition.wait(() -> Bank.isSelectedQuantityAllButton(), 200, 12);
+            Logger.debugLog("Selected Quantity All button.");
+
+            // Selecting the right bank tab again if needed
+            if (!Bank.isSelectedBankTab(banktab)) {
+                Bank.openTab(banktab);
+                Logger.debugLog("Opened bank tab " + banktab);
+            }
+
+            // Withdraw first set of items
+            Bank.withdrawItem(unprocessedItemID, 0.75);
+            Logger.debugLog("Withdrew " + product + " from the bank.");
+
+            // Check if we have the uncuts in the inventory, otherwise stop script.
+            Condition.wait(() -> Inventory.contains(unprocessedItemID, 0.75), 250,10);
+            if (!Inventory.contains(unprocessedItemID, 0.75)) {
+                Logger.log("No " + product + " found in inventory, assuming we're out of items to process.");
+                Bank.close();
+                if (Bank.isOpen()) {
+                    Bank.close();
+                }
+                Logout.logout();
+                Script.forceStop();
+            }
+        } else {
+            // Withdraw first set of items
+            Bank.withdrawItem(unprocessedItemID, 0.75);
+
+            // Check if we have both uncuts and a chisel in the inventory, otherwise stop script.
+            String[] items3 = {chisel, unprocessedItemID};
+            Condition.wait(() -> Inventory.contains(items3, 0.75), 250,10);
+            if (!Inventory.contains(items3, 0.75)) {
+                Logger.log("Not all were items found in inventory, assuming we're out of items to process.");
+                Bank.close();
+                if (Bank.isOpen()) {
+                    Bank.close();
+                }
+                Logout.logout();
+                Script.forceStop();
+            }
+
+            Logger.debugLog("Withdrew " + product + " from the bank.");
+        }
+
+        // Finishing off with closing the bank
+        Logger.debugLog("Closing bank interface.");
+        Bank.close();
+        Logger.debugLog("Closed bank interface.");
+
+        Logger.debugLog("Ending the initialSetup() method.");
+    }
+
+    private void executeCutting() {
+        Logger.debugLog("Starting executeCutting() method.");
+
+        // Check if we have both a chisel and uncuts in the inventory.
+        if (!Inventory.contains(chisel, 0.75) && !Inventory.contains(unprocessedItemID, 0.75)) {
+            Logger.log("We don't have a chisel and " + product + " in our inventory, going back to banking!");
+            return;
+        }
+
+        // Starting to process items
+        Inventory.tapItem(chisel, 0.75);
+        int randomDelay2 = new Random().nextInt(150) + 100;
+        int randomDelay3 = new Random().nextInt(1500) + 500;
+        Condition.sleep(randomDelay2);
+        Inventory.tapItem(unprocessedItemID, 0.75);
+        Logger.debugLog("Waiting for the chatbox Make Menu to be visible...");
+        Condition.wait(() -> Chatbox.isMakeMenuVisible(), 200, 12);
+        Chatbox.makeOption(1);
+        Logger.debugLog("Selected option 1 in chatbox.");
+
+        // Wait for the inventory to finish (with a timeout)
+        long startTime = System.currentTimeMillis();
+        long timeout = 40 * 1000; // 40 seconds in milliseconds as a full invent is about 32 seconds.
+        while (Inventory.contains(unprocessedItemID, 0.75)) {
+            readXP();
+            hopActions();
+            Condition.sleep(randomDelay3);
+
+            // Check if we have passed the timeout
+            if (System.currentTimeMillis() - startTime > timeout) {
+                Logger.debugLog("Timeout reached for inventory.contains() method");
+                break;
+            }
+        }
+        readXP();
+
+        Logger.debugLog("Ending the executeCutting() method.");
+    }
+
+    private void bank() {
+        Logger.debugLog("Starting bank() method.");
+        int randomDelay = new Random().nextInt(250) + 250;
+
+        // Opening the bank based on your location
+        Logger.debugLog("Attempting to open the bank.");
+        Bank.open(bankloc);
+        Logger.debugLog("Bank is open.");
+
+        // Select the right bank tab if needed.
+        if (!Bank.isSelectedBankTab(banktab)) {
+            Bank.openTab(banktab);
+            Logger.debugLog("Opened bank tab " + banktab);
+        }
+
+        // Depositing items based on your product chosen
+        Logger.debugLog("Depositing " + product + ".");
+        Inventory.tapItem(processedItemID, 0.75);
+        Condition.sleep(randomDelay);
+
+        // Check if the product is one of the specific uncut gems
+        if ("Uncut opal".equals(product) || "Uncut jade".equals(product) || "Uncut red topaz".equals(product)) {
+            // Check for and process crushed gems while processing these specific uncuts
+            if (Inventory.contains(crushedgem, 0.75)) {
+                Inventory.tapItem(crushedgem, 0.75);
+                Condition.sleep(randomDelay);
+            }
+        }
+
+        Bank.withdrawItem(unprocessedItemID, 0.75);
+        Logger.debugLog("Withdrew " + product + " from the bank.");
+
+        // Closing the bank, as banking should be done now
+        Bank.close();
+        if(Bank.isOpen()) {
+            Bank.close();
+        }
+        Logger.debugLog("Closed the bank.");
+
+        Logger.debugLog("Ending the bank() method.");
+    }
+
+    private void checkInventOpen() {
+        // Check if the inventory is open (needs this check after a break)
+        if (!GameTabs.isInventoryTabOpen()) {
+            GameTabs.openInventoryTab();
+        }
+    }
+
+    private void checkInventUncuts() {
+        String[] items = {chisel, unprocessedItemID};
+        // Check if we have both a chisel and uncuts in the inventory.
+        if (!Inventory.contains(items, 0.75)) {
+            Logger.log("1st check failed for a chisel and uncuts in our inventory, going back to banking!");
+            bank();
+        }
+
+        // Check if we have both a chisel and uncuts in the inventory.
+        if (!Inventory.contains(items, 0.75)) {
+            Logger.log("2nd check failed for a chisel and uncuts in our inventory, logging out and aborting script!");
+            Logout.logout();
+            Script.forceStop();
+        }
+    }
+
+    private void hopActions() {
+        if(hopEnabled) {
+            Game.hop(hopProfile, useWDH, false);
+        } else {
+            // We do nothing here, as hop is disabled.
+        }
+    }
+
+    private void readXP() {
+        XpBar.getXP();
+    }
+
+}
