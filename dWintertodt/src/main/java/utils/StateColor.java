06e513e5c40c8609fc78d883704463375669e286
Moeinich <danielbang35@gmail.com>
2024-04-24 18:01:34 +0200

    StateUpdater

--- dWintertodt/src/main/java/utils/StateColor.java
+++ dWintertodt/src/main/java/utils/StateColor.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.awt.Color;
+
 public enum StateColor {
     FIRE_ALIVE("#bb3b03"),
     NEEDS_REBURNING("#bb3b03"), // Assuming the negation of presence is handled elsewhere, keeping color same for simplicity
@@ -7,15 +9,15 @@ public enum StateColor {
     MAGE_DEAD("#fe0200"),
     GAME_RED_COLOR("#cc0000");
 
-    private final String colorCode;
+    private final Color color;
 
     // Constructor
     StateColor(String colorCode) {
-        this.colorCode = colorCode;
+        this.color = Color.decode(colorCode);
     }
 
     // Getter
-    public String getColorCode() {
-        return this.colorCode;
+    public Color getColor() {
+        return this.color;
     }
 }
--- dWintertodt/src/main/java/utils/StateUpdater.java
+++ dWintertodt/src/main/java/utils/StateUpdater.java
@@ -3,6 +3,8 @@ package utils;
 
 import java.awt.*;
 
+import static helpers.Interfaces.Client;
+
 public class StateUpdater {
     public void updateStates(WTStates[] states) {
         for (WTStates state : states) {
@@ -16,21 +18,29 @@ public class StateUpdater {
 
     private boolean updateFireAlive(WTStates state) {
         Rectangle checkRect = state.getRectangle();
-        return false;
+        Color checkColor = StateColor.FIRE_ALIVE.getColor();
+
+        return Client.isColorInRect(checkColor, checkRect, 10);
     }
 
     private boolean updateNeedsReburning(WTStates state) {
         Rectangle checkRect = state.getRectangle();
-        return false;
+        Color checkColor = StateColor.NEEDS_REBURNING.getColor();
+
+        return Client.isColorInRect(checkColor, checkRect, 10);
     }
 
     private boolean updateNeedsFixing(WTStates state) {
         Rectangle checkRect = state.getRectangle();
-        return false;
+        Color checkColor = StateColor.NEEDS_FIXING.getColor();
+
+        return Client.isColorInRect(checkColor, checkRect, 10);
     }
 
     private boolean updateMageDead(WTStates state) {
         Rectangle checkRect = state.getRectangle();
-        return false;
+        Color checkColor = StateColor.MAGE_DEAD.getColor();
+
+        return Client.isColorInRect(checkColor, checkRect, 10);
     }
 }
