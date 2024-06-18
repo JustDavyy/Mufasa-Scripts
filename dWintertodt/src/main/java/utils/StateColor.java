ac2af44debbba95ec7e1fdb929dafaf464b506e7
Moeinich <danielbang35@gmail.com>
2024-06-18 14:22:04 +0200

    Update StateColor.java

--- dWintertodt/src/main/java/utils/StateColor.java
+++ dWintertodt/src/main/java/utils/StateColor.java
@@ -7,7 +7,8 @@ public enum StateColor {
     NEEDS_REBURNING("#dd4f01"), // Assuming the negation of presence is handled elsewhere, keeping color same for simplicity
     NEEDS_FIXING("#777777"),
     MAGE_DEAD("#fe0200"),
-    GAME_RED_COLOR("#cc0000");
+    GAME_RED_COLOR("#cc0000"),
+    GAME_GREEN_COLOR("#00cc00");
 
     private final Color color;
 
