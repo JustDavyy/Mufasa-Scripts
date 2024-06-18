4635aa36c3360f2711eb87aabda3cf11f96f2854
Moeinich <danielbang35@gmail.com>
2024-06-18 13:21:52 +0200

    Update BurnBranches.java

--- dWintertodt/src/main/java/tasks/BurnBranches.java
+++ dWintertodt/src/main/java/tasks/BurnBranches.java
@@ -34,7 +34,6 @@ public class BurnBranches extends Task {
             Walker.step(SideManager.getBurnTile(), WTRegion);
             Player.waitTillNotMoving(10, WTRegion);
             currentLocation = Walker.getPlayerPosition(WTRegion);
-
         }
 
         if (Player.atTile(SideManager.getBurnTile(), dWintertodt.WTRegion)) {
