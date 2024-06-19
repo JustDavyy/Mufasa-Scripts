6f2af0d5d33778d24c1e532cbe0ac4114d1ac38c
Davy <davykanningstudent@gmail.com>
2024-06-19 17:08:41 +0200

    Update StateUpdater.java

--- dWintertodt/src/main/java/utils/StateUpdater.java
+++ dWintertodt/src/main/java/utils/StateUpdater.java
@@ -127,7 +127,7 @@ public class StateUpdater {
     }
 
     private static void updateShouldBurn() {
-        shouldBurn = (gameAt20Percent && (inventoryHasKindlings || inventoryHasBruma) && Inventory.usedSlots() >= 18);
+        shouldBurn = (gameAt20Percent && (inventoryHasKindlings || inventoryHasBruma) && Inventory.usedSlots() >= 18 || isGameGoing && gameAt20Percent && inventoryHasBruma);
     }
 
     private static void updateKindlingState() {
