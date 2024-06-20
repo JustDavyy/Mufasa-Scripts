b048f5973f70b822b81dd077772b8d50270aed87
Moeinich <danielbang35@gmail.com>
2024-06-20 08:33:59 +0200

    some on switchside?

--- dWintertodt/src/main/java/tasks/SwitchSide.java
+++ dWintertodt/src/main/java/tasks/SwitchSide.java
@@ -8,6 +8,7 @@ import static helpers.Interfaces.*;
 import static main.dWintertodt.*;
 
 public class SwitchSide extends Task {
+    private boolean isMageDead = false;
     @Override
     public boolean activate() {
         //Logger.debugLog("Inside SwitchSide activate()");
@@ -15,12 +16,15 @@ public class SwitchSide extends Task {
         if (SideManager.getMageDead() && Player.tileEquals(currentLocation, SideManager.getBurnTile())) {
 
             // Check for 5 seconds if the mage is dead for at least 5 seconds
-            Condition.wait(() -> SideManager.isMageDeadForAtLeast(5), 250, 20);
+            Condition.wait(() -> {
+                if (SideManager.isMageDeadForAtLeast(5)) {
+                    isMageDead = true;
+                    return true;
+                }
+            return isMageDead;}, 250, 20);
 
-            return SideManager.isMageDeadForAtLeast(5);
-        } else {
-            return false;
         }
+        return isMageDead;
     }
 
     @Override
@@ -41,6 +45,7 @@ public class SwitchSide extends Task {
                 // Step to the branch tile
                 Walker.step(SideManager.getBranchTile(), WTRegion);
                 currentLocation = SideManager.getBranchTile();
+                isMageDead = false;
             }
             // Check if we are on the burn tile
             else if (Player.tileEquals(currentLocation, SideManager.getBurnTile())) {
@@ -53,12 +58,14 @@ public class SwitchSide extends Task {
                 // Step to the burn tile
                 Walker.step(SideManager.getBurnTile(), WTRegion);
                 currentLocation = SideManager.getBurnTile();
+                isMageDead = false;
             }
             // Else walk to the middle if we are not found on any of the two tiles
             else {
                 currentSide = "Right";
                 Walker.walkTo(new Tile(638, 167), WTRegion);
                 currentLocation = Walker.getPlayerPosition(WTRegion);
+                isMageDead = false;
             }
         // Logic if we're currently on the right side
         } else if (currentSide.equals("Right")) {
@@ -73,6 +80,7 @@ public class SwitchSide extends Task {
                 // Step to the branch tile
                 Walker.step(SideManager.getBranchTile(), WTRegion);
                 currentLocation = SideManager.getBranchTile();
+                isMageDead = false;
             }
             // Check if we are on the burn tile
             else if (Player.tileEquals(currentLocation, SideManager.getBurnTile())) {
@@ -85,17 +93,20 @@ public class SwitchSide extends Task {
                 // Step to the burn tile
                 Walker.step(SideManager.getBurnTile(), WTRegion);
                 currentLocation = SideManager.getBurnTile();
+                isMageDead = false;
             }
             // Else walk to the middle if we are not found on any of the two tiles
             else {
                 currentSide = "Left";
                 Walker.walkTo(new Tile(638, 167), WTRegion);
                 currentLocation = Walker.getPlayerPosition(WTRegion);
+                isMageDead = false;
             }
         } else {
             // For some dumb fucking reason we're bot not on the left, and not on the right?
             Walker.walkTo(new Tile(638, 167), WTRegion);
             currentLocation = Walker.getPlayerPosition(WTRegion);
+            isMageDead = false;
         }
 
         return false;
