94f91f6d5108c4dff851af6cb13fa8095e6f9ca9
Davy <davykanningstudent@gmail.com>
2024-06-22 00:05:51 +0200

    removed timer logs

--- dmWinterbodt/src/main/java/utils/StateUpdater.java
+++ dmWinterbodt/src/main/java/utils/StateUpdater.java
@@ -173,8 +173,6 @@ public class StateUpdater {
     }
 
     private static void updateShouldBurn() {
-        long startTime = System.currentTimeMillis(); // Start time
-
         shouldBurn = (
                 // for regular mode
                 gameAt20Percent && (inventoryHasKindlings || inventoryHasLogs) && Inventory.usedSlots() >= 18) && !burnOnly
@@ -182,23 +180,11 @@ public class StateUpdater {
                 // for burn only!
                 || burnOnly && isGameGoing && inventoryHasLogs && Inventory.isFull()
                 || burnOnly && isGameGoing && inventoryHasLogs && gameAt20Percent;
-
-        long endTime = System.currentTimeMillis(); // End time
-        long duration = endTime - startTime; // Duration in milliseconds
-
-        Logger.log("Time taken to execute updateShouldBurn: " + duration + " ms");
     }
 
     private static void updateKindlingState() {
-        long startTime = System.currentTimeMillis(); // Start time
-
         inventoryHasKindlings = Inventory.contains(brumaKindling, 0.8);
         inventoryHasLogs = Inventory.contains(brumaRoot, 0.8);
-
-        long endTime = System.currentTimeMillis(); // End time
-        long duration = endTime - startTime; // Duration in milliseconds
-
-        Logger.log("Time taken to execute updateKindlingState: " + duration + " ms");
     }
 
     private static void updateWaitingForGameToStart() {
