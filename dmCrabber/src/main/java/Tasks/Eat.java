417be8e05e478b1bf4276d050cf7eda8c1601f5c
Shadiezz <danielbang35@gmail.com>
2024-07-02 17:00:58 +0200

    Update Eat.java

--- dmCrabber/src/main/java/Tasks/Eat.java
+++ dmCrabber/src/main/java/Tasks/Eat.java
@@ -2,14 +2,42 @@ package Tasks;
 
 import utils.Task;
 
+import static helpers.Interfaces.*;
+import static main.dmCrabber.*;
+
 public class Eat extends Task {
-    @Override
     public boolean activate() {
-        return false;
+        return hpToEat > Player.getHP();
     }
 
     @Override
     public boolean execute() {
-        return false;
+        Logger.debugLog("Inside Eat execute()");
+
+        if (!GameTabs.isInventoryTabOpen()) {
+            GameTabs.openInventoryTab();
+        }
+
+        if (java.util.Objects.equals(selectedFood, "Cakes")) {
+            // cake IDs; 1891, 1893, 1895
+            if (Inventory.contains(1895, 0.75)) { // slice of cake
+                Inventory.eat(1895, 0.75);
+                Condition.wait(() -> Player.getHP() > hpToEat, 200, 20);
+                return true;
+            } else if (Inventory.contains(1893, 0.75)) { // 2/3 cake
+                Inventory.eat(1893, 0.75);
+                Condition.wait(() -> Player.getHP() > hpToEat, 200, 20);
+                return true;
+            } else if (Inventory.contains(1891, 0.75)) { // full cake
+                Inventory.eat(1891, 0.75);
+                Condition.wait(() -> Player.getHP() > hpToEat, 200, 20);
+                return true;
+            }
+            return false;
+        } else {
+            Inventory.eat(foodID, 0.75);
+            Condition.wait(() -> Player.getHP() > hpToEat, 200, 20);
+            return true;
+        }
     }
 }
