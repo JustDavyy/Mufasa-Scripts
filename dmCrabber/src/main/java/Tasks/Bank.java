4cfc547abf7e653ecfb3b388f7c6eac21cbc3686
Shadiezz <danielbang35@gmail.com>
2024-07-02 17:10:38 +0200

    Update Bank.java

--- dmCrabber/src/main/java/Tasks/Bank.java
+++ dmCrabber/src/main/java/Tasks/Bank.java
@@ -6,6 +6,7 @@ import static helpers.Interfaces.Logger;
 
 public class Bank extends Task {
 
+    // I'm guessing we should just withdraw full inv of food?
     @Override
     public boolean activate() {
         return false;
