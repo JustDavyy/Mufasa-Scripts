9570871074ce4c987e283b9b1af3f088d6f95655
Moeinich <danielbang35@gmail.com>
2024-07-22 14:55:59 +0200

    version bump

--- ezMuseumCleaner/src/main/java/main/ezMuseumCleaner.java
+++ ezMuseumCleaner/src/main/java/main/ezMuseumCleaner.java
@@ -20,7 +20,7 @@ import static helpers.Interfaces.*;
 @ScriptManifest(
         name = "ezMuseumCleaner",
         description = "Does Museum Cleaning for all your off-skill needs",
-        version = "1.0",
+        version = "1.1",
         guideLink = "https://wiki.mufasaclient.com/docs/ezmuseumcleaner/",
         categories = {ScriptCategory.Minigames}
 )
