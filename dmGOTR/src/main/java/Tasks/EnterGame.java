c6cce542a66e237158355cfbff1156e0bbe9fbcd
Moeinich <danielbang35@gmail.com>
2024-07-30 13:03:12 +0200

    Create EnterGame.java

---
+++ dmGOTR/src/main/java/Tasks/EnterGame.java
@@ -0,0 +1,16 @@
+package Tasks;
+
+import utils.Task;
+
+public class EnterGame extends Task {
+    // Color finder parameters to check the portal color!
+    @Override
+    public boolean activate() {
+        return false;
+    }
+
+    @Override
+    public boolean execute() {
+        return false;
+    }
+}
