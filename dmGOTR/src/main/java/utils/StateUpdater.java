90d1e057a58226630afc83b8327bb79fd1299474
Moeinich <danielbang35@gmail.com>
2024-07-30 13:04:27 +0200

    Update StateUpdater.java

--- dmGOTR/src/main/java/utils/StateUpdater.java
+++ dmGOTR/src/main/java/utils/StateUpdater.java
@@ -1,4 +1,16 @@
 package utils;
 
 public class StateUpdater {
+
+    public void updateAllStates() {
+        // Update all states
+    }
+
+    public void resetAllStates() {
+        // Reset all states!
+    }
+
+    //Set each state individually
+
+    //Getters for each state
 }
