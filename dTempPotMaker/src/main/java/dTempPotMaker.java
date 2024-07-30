e611e03de56f011e11cdcfef3f8d1cb88b44e3f2
Davy <davykanningstudent@gmail.com>
2024-07-30 09:26:12 +0200

    pot maker

--- dTempPotMaker/src/main/java/dTempPotMaker.java
+++ dTempPotMaker/src/main/java/dTempPotMaker.java
@@ -1,4 +1,5 @@
 import helpers.*;
+import helpers.annotations.AllowedValue;
 import helpers.annotations.ScriptConfiguration;
 import helpers.annotations.ScriptManifest;
 import helpers.utils.ItemList;
@@ -11,14 +12,24 @@ import static helpers.Interfaces.*;
 import static helpers.Interfaces.Logout;
 
 @ScriptManifest(
-        name = "dTempRestoreMaker",
-        description = "Makes super restores in any of the dynamic banks.",
-        version = "1.0",
+        name = "dTempPotMaker",
+        description = "Makes super restores/attacks in any of the dynamic banks.",
+        version = "1.1",
         guideLink = "",
         categories = {ScriptCategory.Herblore}
 )
 @ScriptConfiguration.List(
         {
+                @ScriptConfiguration(
+                        name =  "Product",
+                        description = "What potion would you like to make?",
+                        defaultValue = "Super Restore",
+                        allowedValues = {
+                                @AllowedValue(optionIcon = "145", optionName = "Super Attack"),
+                                @AllowedValue(optionIcon = "3026", optionName = "Super Restore")
+                        },
+                        optionType = OptionType.STRING
+                ),
                 @ScriptConfiguration(
                         name =  "Bank Tab",
                         description = "What bank tab are your resources located in?",
@@ -34,36 +45,44 @@ import static helpers.Interfaces.Logout;
         }
 )
 
-public class dTempRestoreMaker extends AbstractScript {
+public class dTempPotMaker extends AbstractScript {
     private final Random random = new Random();
     // Creating the strings for later use
     String hopProfile;
     Boolean hopEnabled;
     Boolean useWDH;
     String bankloc;
+    String product;
     int banktab;
     int processCount = 0;
     int productIndex;
-    int[] reqItems = new int[]{ItemList.SNAPDRAGON_POTION_UNF_3004, ItemList.RED_SPIDERS_EGGS_223};
+    int[] reqItemsRest = new int[]{ItemList.SNAPDRAGON_POTION_UNF_3004, ItemList.RED_SPIDERS_EGGS_223};
+    int[] reqItemsAttk = new int[]{ItemList.IRIT_POTION_UNF_101, ItemList.EYE_OF_NEWT_221};
 
     // This is the onStart, and only gets ran once.
     @Override
     public void onStart(){
         Map<String, String> configs = getConfigurations();
+        product = configs.get("Product");
         banktab = Integer.parseInt(configs.get("Bank Tab"));
         hopProfile = (configs.get("Use world hopper?"));
         hopEnabled = Boolean.valueOf((configs.get("Use world hopper?.enabled")));
         useWDH = Boolean.valueOf((configs.get("Use world hopper?.useWDH")));
 
         //Logs for debugging purposes
-        Logger.log("Thank you for using the dTempRestoreMaker script!");
+        Logger.log("Thank you for using the dTempPotMaker script!");
 
         // Creating the Paint object
         Logger.debugLog("Creating paint object.");
         Paint.Create("/logo/davyy.png");
 
         // Create a single image box, to show the amount of processed bows
-        productIndex = Paint.createBox("Super restore", ItemList.SUPER_RESTORE4_3024, processCount);
+        if (product.equals("Super Restore")) {
+            productIndex = Paint.createBox("Super Restore", ItemList.SUPER_RESTORE3_3026, processCount);
+        } else {
+            productIndex = Paint.createBox("Super Attack", ItemList.SUPER_ATTACK3_145, processCount);
+        }
+
 
         // Set the two top headers of paintUI.
         Paint.setStatus("Initializing...");
@@ -162,10 +181,17 @@ public class dTempRestoreMaker extends AbstractScript {
         }
 
         // Withdraw first set of items
-        Bank.withdrawItem(ItemList.SNAPDRAGON_POTION_UNF_3004, 0.8);
-        Condition.sleep(generateDelay(250, 400));
-        Bank.withdrawItem(ItemList.RED_SPIDERS_EGGS_223, 0.8);
-        Condition.sleep(generateDelay(150, 225));
+        if (product.equals("Super Restore")) {
+            Bank.withdrawItem(ItemList.SNAPDRAGON_POTION_UNF_3004, 0.8);
+            Condition.sleep(generateDelay(250, 400));
+            Bank.withdrawItem(ItemList.RED_SPIDERS_EGGS_223, 0.8);
+            Condition.sleep(generateDelay(150, 225));
+        } else {
+            Bank.withdrawItem(ItemList.IRIT_POTION_UNF_101, 0.8);
+            Condition.sleep(generateDelay(250, 400));
+            Bank.withdrawItem(ItemList.EYE_OF_NEWT_221, 0.8);
+            Condition.sleep(generateDelay(150, 225));
+        }
 
         // Finishing off with closing the bank
         Logger.debugLog("Closing bank interface.");
@@ -184,16 +210,30 @@ public class dTempRestoreMaker extends AbstractScript {
         Logger.debugLog("Starting executeMakePotMethod() method.");
         Paint.setStatus("Execute make pots");
 
-        // Check if we have both a knife and the logs in the inventory.
-        if (!Inventory.contains(ItemList.SNAPDRAGON_POTION_UNF_3004, 0.75) && !Inventory.contains(ItemList.RED_SPIDERS_EGGS_223, 0.75)) {
-            Logger.log("We don't have a UNFINISHED POTIONS and SPIDER EGGS in our inventory, going back to banking!");
-            return;
+        // Check if we have all supplies in the inventory.
+        if (product.equals("Super Restore")) {
+            if (!Inventory.containsAll(reqItemsRest, 0.75)) {
+                Logger.log("We don't have UNFINISHED POTIONS and SPIDER EGGS in our inventory, going back to banking!");
+                return;
+            }
+        } else {
+            if (!Inventory.containsAll(reqItemsAttk, 0.75)) {
+                Logger.log("We don't have UNFINISHED POTIONS and EYE OF NEWT in our inventory, going back to banking!");
+                return;
+            }
         }
 
         // Starting to process items
-        Inventory.tapItem(ItemList.SNAPDRAGON_POTION_UNF_3004, 0.75);
-        Condition.sleep(generateDelay(200, 350));
-        Inventory.tapItem(ItemList.RED_SPIDERS_EGGS_223, 0.75);
+        if (product.equals("Super Restore")) {
+            Inventory.tapItem(ItemList.SNAPDRAGON_POTION_UNF_3004, 0.75);
+            Condition.sleep(generateDelay(200, 350));
+            Inventory.tapItem(ItemList.RED_SPIDERS_EGGS_223, 0.75);
+        } else {
+            Inventory.tapItem(ItemList.IRIT_POTION_UNF_101, 0.75);
+            Condition.sleep(generateDelay(200, 350));
+            Inventory.tapItem(ItemList.EYE_OF_NEWT_221, 0.75);
+        }
+
         Logger.debugLog("Waiting for the chatbox Make Menu to be visible...");
         Condition.wait(() -> Chatbox.isMakeMenuVisible(), 200, 12);
         Chatbox.makeOption(1);
@@ -202,26 +242,51 @@ public class dTempRestoreMaker extends AbstractScript {
         long startTime = System.currentTimeMillis();
         long timeout = 18 * 1000; // 18 seconds in milliseconds as a full invent is about 17 seconds.
         Paint.setStatus("Waiting for completion");
-        while (Inventory.containsAll(reqItems, 0.8)) {
-            readXP();
-            Condition.sleep(generateDelay(500, 750));
-            hopActions();
+        if (product.equals("Super Restore")) {
+            while (Inventory.containsAll(reqItemsRest, 0.8)) {
+                readXP();
+                Condition.sleep(generateDelay(500, 750));
+                hopActions();
 
-            if (Player.leveledUp()) {
-                startTime = System.currentTimeMillis();
-                // Starting to process items
-                Inventory.tapItem(ItemList.SNAPDRAGON_POTION_UNF_3004, 0.75);
-                Condition.sleep(generateDelay(200, 350));
-                Inventory.tapItem(ItemList.RED_SPIDERS_EGGS_223, 0.75);
-                Logger.debugLog("Waiting for the chatbox Make Menu to be visible...");
-                Condition.wait(() -> Chatbox.isMakeMenuVisible(), 200, 12);
-                Chatbox.makeOption(1);
+                if (Player.leveledUp()) {
+                    startTime = System.currentTimeMillis();
+                    // Starting to process items
+                    Inventory.tapItem(ItemList.SNAPDRAGON_POTION_UNF_3004, 0.75);
+                    Condition.sleep(generateDelay(200, 350));
+                    Inventory.tapItem(ItemList.RED_SPIDERS_EGGS_223, 0.75);
+                    Logger.debugLog("Waiting for the chatbox Make Menu to be visible...");
+                    Condition.wait(() -> Chatbox.isMakeMenuVisible(), 200, 12);
+                    Chatbox.makeOption(1);
+                }
+
+                // Check if we have passed the timeout
+                if (System.currentTimeMillis() - startTime > timeout) {
+                    Logger.debugLog("Timeout reached for inventory.contains() method");
+                    break;
+                }
             }
+        } else {
+            while (Inventory.containsAll(reqItemsAttk, 0.8)) {
+                readXP();
+                Condition.sleep(generateDelay(500, 750));
+                hopActions();
 
-            // Check if we have passed the timeout
-            if (System.currentTimeMillis() - startTime > timeout) {
-                Logger.debugLog("Timeout reached for inventory.contains() method");
-                break;
+                if (Player.leveledUp()) {
+                    startTime = System.currentTimeMillis();
+                    // Starting to process items
+                    Inventory.tapItem(ItemList.IRIT_POTION_UNF_101, 0.75);
+                    Condition.sleep(generateDelay(200, 350));
+                    Inventory.tapItem(ItemList.EYE_OF_NEWT_221, 0.75);
+                    Logger.debugLog("Waiting for the chatbox Make Menu to be visible...");
+                    Condition.wait(() -> Chatbox.isMakeMenuVisible(), 200, 12);
+                    Chatbox.makeOption(1);
+                }
+
+                // Check if we have passed the timeout
+                if (System.currentTimeMillis() - startTime > timeout) {
+                    Logger.debugLog("Timeout reached for inventory.contains() method");
+                    break;
+                }
             }
         }
         readXP();
@@ -250,9 +315,15 @@ public class dTempRestoreMaker extends AbstractScript {
         Bank.tapDepositInventoryButton();
         Condition.sleep(generateDelay(300, 500));
 
-        Bank.withdrawItem(ItemList.SNAPDRAGON_POTION_UNF_3004, 0.8);
-        Condition.sleep(generateDelay(250, 400));
-        Bank.withdrawItem(ItemList.RED_SPIDERS_EGGS_223, 0.8);
+        if (product.equals("Super Restore")) {
+            Bank.withdrawItem(ItemList.SNAPDRAGON_POTION_UNF_3004, 0.8);
+            Condition.sleep(generateDelay(250, 400));
+            Bank.withdrawItem(ItemList.RED_SPIDERS_EGGS_223, 0.8);
+        } else {
+            Bank.withdrawItem(ItemList.IRIT_POTION_UNF_101, 0.8);
+            Condition.sleep(generateDelay(250, 400));
+            Bank.withdrawItem(ItemList.EYE_OF_NEWT_221, 0.8);
+        }
         Condition.sleep(generateDelay(150, 225));
 
         // Closing the bank, as banking should be done now
@@ -276,15 +347,28 @@ public class dTempRestoreMaker extends AbstractScript {
     private void checkInventPotMethod() {
         Paint.setStatus("Check invent for items");
 
-        if (!Inventory.containsAll(reqItems, 0.75)) {
-            Logger.debugLog("1st check failed for required items in our inventory, going back to banking!");
-            bank();
-        }
+        if (product.equals("Super Restore")) {
+            if (!Inventory.containsAll(reqItemsRest, 0.75)) {
+                Logger.debugLog("1st check failed for required items in our inventory, going back to banking!");
+                bank();
+            }
 
-        if (!Inventory.containsAll(reqItems, 0.75)) {
-            Logger.log("2nd check failed for required items in our inventory, logging out and aborting script!");
-            Logout.logout();
-            Script.stop();
+            if (!Inventory.containsAll(reqItemsRest, 0.75)) {
+                Logger.log("2nd check failed for required items in our inventory, logging out and aborting script!");
+                Logout.logout();
+                Script.stop();
+            }
+        } else {
+            if (!Inventory.containsAll(reqItemsAttk, 0.75)) {
+                Logger.debugLog("1st check failed for required items in our inventory, going back to banking!");
+                bank();
+            }
+
+            if (!Inventory.containsAll(reqItemsAttk, 0.75)) {
+                Logger.log("2nd check failed for required items in our inventory, logging out and aborting script!");
+                Logout.logout();
+                Script.stop();
+            }
         }
     }
 
