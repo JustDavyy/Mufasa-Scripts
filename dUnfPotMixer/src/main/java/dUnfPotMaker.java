ce0cb9c6af621812c1efc86faf91cd376eaee5b0
Davy <davykanningstudent@gmail.com>
2024-09-08 11:41:10 +0200

    dUnfPotMaker update

--- dUnfPotMixer/src/main/java/dUnfPotMaker.java
+++ dUnfPotMixer/src/main/java/dUnfPotMaker.java
@@ -15,7 +15,7 @@ import java.util.Random;
 @ScriptManifest(
         name = "dUnfPotMixer",
         description = "Mixes all your herbs to unfinished potions, supports dynamic banking and world hops.",
-        version = "1.022",
+        version = "1.03",
         guideLink = "https://wiki.mufasaclient.com/docs/dunfinished-potion-maker/",
         categories = {ScriptCategory.Herblore, ScriptCategory.Moneymaking}
 )
