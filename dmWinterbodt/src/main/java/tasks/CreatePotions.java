d23a952c917f9cc4be4fe95843577880fbf8a307
Moeinich <danielbang35@gmail.com>
2024-10-09 14:51:36 +0200

    Create CreatePotions.java

---
+++ dmWinterbodt/src/main/java/tasks/CreatePotions.java
@@ -0,0 +1,15 @@
+package tasks;
+
+import utils.Task;
+
+public class CreatePotions extends Task {
+    @Override
+    public boolean activate() {
+        return false;
+    }
+
+    @Override
+    public boolean execute() {
+        return false;
+    }
+}
