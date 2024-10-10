2747f616e78e43f8fffa01d460a0aaac6677dc29
Davy <davykanningstudent@gmail.com>
2024-10-10 21:27:10 +0200

    bump version

--- dMinnows Fisher/src/main/java/dMinnowsFisher.java
+++ dMinnows Fisher/src/main/java/dMinnowsFisher.java
@@ -14,7 +14,7 @@ import static helpers.Interfaces.*;
 @ScriptManifest(
         name = "dMinnows Fisher",
         description = "Fishes Minnows at Kylie Minnow's fishing platform at the Fishing Guild. The angler's outfit is needed to unlock the platform.",
-        version = "1.03",
+        version = "1.05",
         guideLink = "https://wiki.mufasaclient.com/docs/dminnows-fisher/",
         categories = {ScriptCategory.Fishing}
 )
