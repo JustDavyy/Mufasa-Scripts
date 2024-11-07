713ede65ea5e9c3547b0d47814b432713512e6ca
Davy <davykanningstudent@gmail.com>
2024-11-07 22:18:56 +0100

    seers minor tweak again

--- dAgility/src/main/java/dAgility/Tasks/Seers.java
+++ dAgility/src/main/java/dAgility/Tasks/Seers.java
@@ -127,6 +127,9 @@ public class Seers extends Task {
                 if (obstacle.checkForMark && obstacle.markHandling != null) {
                     for (MarkHandling mark : obstacle.markHandling) {
                         Condition.sleep(generateRandomDelay(200, 400));
+                        if (obstacle.name.equals("Obstacle 2")) {
+                            Condition.sleep(generateRandomDelay(200, 300));
+                        }
                         if (mark.isMarkPresent(mark.checkArea, mark.targetColor)) {
                             Paint.setStatus("Pick up mark of grace");
                             Logger.log("Mark of grace detected, picking it up!");
