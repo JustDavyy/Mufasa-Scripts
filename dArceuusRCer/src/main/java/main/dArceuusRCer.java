14bbf8b9c12685b8232c8beb2c3ee6da4af861b4
Davy <davykanningstudent@gmail.com>
2024-12-25 12:20:12 +0100

    additional color added

--- dArceuusRCer/src/main/java/main/dArceuusRCer.java
+++ dArceuusRCer/src/main/java/main/dArceuusRCer.java
@@ -28,7 +28,7 @@ import static helpers.Interfaces.*;
 @ScriptManifest(
         name = "dArceuus RCer",
         description = "Crafts blood or soul runes at Arceuus, supports using blood essence and hopping worlds. DISCLAIMER: This script is NOT fully safe for 10HP accounts, use at own risk!",
-        version = "2.13",
+        version = "2.14",
         guideLink = "https://wiki.mufasaclient.com/docs/darceuus-rcer/",
         categories = {ScriptCategory.Runecrafting, ScriptCategory.Moneymaking}
 )
