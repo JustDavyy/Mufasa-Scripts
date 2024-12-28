844cad27fdf66564632fdc7f0f8080ebc902af0d
Moeinich <danielbang35@gmail.com>
2024-12-28 16:01:27 +0100

    Create Course.java

---
+++ agi_sdk/src/main/java/agi_sdk/helpers/Course.java
@@ -0,0 +1,16 @@
+package agi_sdk.helpers;
+
+public enum Course {
+    ADVANCED_WYRN,
+    AL_KHARID,
+    ARDOUGNE,
+    BASIC_WYRM,
+    CANIFIS,
+    DRAYNOR,
+    FALADOR,
+    GNOME,
+    POLLNIVEACH,
+    RELLEKKA,
+    SEERS,
+    VARROCK
+}
