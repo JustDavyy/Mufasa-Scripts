5544a0e2302553cb7505fda0fe247d20d3bb3f83
Davy <davykanningstudent@gmail.com>
2025-01-08 21:07:08 +0100

    Update StateUpdater.java

--- dmGOTR/src/main/java/utils/StateUpdater.java
+++ dmGOTR/src/main/java/utils/StateUpdater.java
@@ -47,7 +47,6 @@ public class StateUpdater {
 
             // Check if the rune color is in the rectangle
             if (Client.isColorInRect(rune.getColor(), ELEMENTAL_RUNE_RECT, 5)) {
-                Logger.debugLog("Current active elemental rune: " + rune.getName());
                 setElementalRune(rune);
                 matchFound = true;
                 break; // Exit loop after finding a match
@@ -74,15 +73,12 @@ public class StateUpdater {
             if (Client.isColorInRect(rune.getColor(), CATALYTIC_RUNE_RECT, 5)) {
                 if (rune == RuneInfo.LAW || rune == RuneInfo.BODY) {
                     // Specific block for LAW and BODY
-                    Logger.debugLog("Current active catalytic rune is LAW or BODY: " + rune.getName());
-                    handleLawOrBodyRune(rune);
+                    handleLawOrBodyRune();
                 } else if (rune == RuneInfo.MIND || rune == RuneInfo.CHAOS) {
                     // Specific block for MIND and CHAOS
-                    Logger.debugLog("Current active catalytic rune is MIND or CHAOS: " + rune.getName());
-                    handleMindOrChaosRune(rune);
+                    handleMindOrChaosRune();
                 } else {
                     // General handling for other catalytic runes
-                    Logger.debugLog("Current active catalytic rune: " + rune.getName());
                     setCatalyticRune(rune);
                 }
                 matchFound = true;
@@ -151,7 +147,7 @@ public class StateUpdater {
 
 
     // Specific handling methods for LAW/BODY and MIND/CHAOS
-    private void handleLawOrBodyRune(RuneInfo rune) {
+    private void handleLawOrBodyRune() {
         if (Client.getPointsFromColorsInRect(RuneInfo.LAW.getColorAsList(), CATALYTIC_RUNE_RECT, 5).size() > 100) {
             setCatalyticRune(RuneInfo.LAW);
         } else {
@@ -159,7 +155,7 @@ public class StateUpdater {
         }
     }
 
-    private void handleMindOrChaosRune(RuneInfo rune) {
+    private void handleMindOrChaosRune() {
         if (Client.isColorInRect(Color.decode("#ffffff"), CATALYTIC_RUNE_RECT, 5)) {
             setCatalyticRune(RuneInfo.CHAOS);
         } else {
